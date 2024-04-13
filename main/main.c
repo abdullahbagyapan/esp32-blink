bfda876bc9a39bffd9e03b095a95bcf6f066236b
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-13 21:14:42 +0300

    update::main.c
    -Add main_delay() function
    
    -Import LED module

--- main/main.c
+++ main/main.c
@@ -1,6 +1,66 @@
+/*
+ @file:   main.c
+ @author: Abdullah Bagyapan
+ @date:   13/04/2024
+*/
+
+
+/*================================== Libraries ==================================*/
+
+
+// Other modules
+#include "LED/led.h"
+
+// Standard libraries
 #include <stdio.h>
+
+
+/*================================== Prototypes ==================================*/
+
+
+void main_delay(volatile uint32_t);
+
+
+/*================================== Functions ==================================*/
+
+
+/*
+ @brief:  Delay
+
+ @author  Abdullah Bagyapan
+
+ @date    13/04/2024
+
+ @todo    Delay in Ms(millisecond)
+
+ @param   None
+
+ @return  None
+*/
+void main_delay(volatile uint32_t ui32DelayCounter) {
+
+    while (ui32DelayCounter != 0) {
+
+        ui32DelayCounter--;
+    }
+
+}
+
 
 void app_main(void)
 {
+
+    LED_Init();
+
+    while (1) {
+
+        LED_On();
+        main_delay(8000000);
+
+        LED_Off();
+        main_delay(8000000);
+
+    }
+
 
 }
